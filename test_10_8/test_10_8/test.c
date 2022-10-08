c96fd080d64c02bacfcf9a64ccab602d33784f1a
D-ZX-LS <1626327156@qq.com>
2022-10-08 23:35:24 +0800

    OJ练习

---
+++ test_10_8/test_10_8/test.c
@@ -0,0 +1,125 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	printf("The first C Program\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b;
+//	scanf("%d%d", &a, &b);
+//	int add = a + b;
+//	printf("%d\n", add);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	if (a > b)
+//		printf("%d\n", a);
+//	else
+//		printf("%d\n", b);
+//	return 0;
+//}
+
+//#include "stdio.h"
+//main()
+//{
+//    int a, b;/*定义整型变量a和b*/
+//    double i, j;/*定义浮点变量i和j*/
+//        a = 5;
+//    b = 6;
+//    i = 3.14; j = i * a * b;
+//    printf("a=%d,b=%d,i=%.2f,j=%.2f\n", a, b, i, j);
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	printf("C:\\ABC.TXT");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	float d = 15, e, f;
+//	a = 35 % 7;
+//	b = 15 / 10;
+//	c = b++;
+//	e = 15 / 10;
+//	f = d / 10;
+//	printf("%d,%d,%d,%f,%f,%f", a, b, c, d, e, f);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char a, b;
+//	a = 330;
+//	b = '\141';
+//	printf("%c,%c", a, b);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char ch = getchar();
+//	printf("%c", ch);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int c = a + b;
+//	printf("%d\n", c);
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	double pi = 3.14159;
+//	double r;
+//	scanf("%lf", &r);
+//	double S = pi * r * r;
+//	printf("%.2lf", S);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	double f = 0;
+//	scanf("%lf", &f);
+//	double c = 5.0 / 9.0 * (f - 32);
+//	printf("%.2lf", c);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int b = a / 100;
+//	int c = a % 10;
+//	printf("%d\n%d\n", b, c);
+//	return 0;
+//}
+
